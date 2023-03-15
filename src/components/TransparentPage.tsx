c68e71a3e7f0636eb3f6637288df51ddf56815aa
Ashutosh <mathoreashutosh@gmail.com>
2023-03-16 00:59:46 +0530

    intital commit

--- src/components/TransparentPage.tsx
+++ src/components/TransparentPage.tsx
@@ -6,13 +6,6 @@ const TransparentPage: React.FC = () => {
   const size = useWindowSize();
   return (
     <>
-      <Webcam
-        audio={false}
-        style={{ position: "fixed", top: 0, left: 0, zIndex: -1 }}
-        height={size.height}
-        width={size.width}
-        videoConstraints={{ facingMode: "environment" }}
-      />
       <div
         style={{
           position: "absolute",
@@ -20,11 +13,35 @@ const TransparentPage: React.FC = () => {
           left: 0,
           width: "100vw",
           height: "100vh",
-          background: "rgba(0, 0, 0, 0.5)",
-          fontSize: 60,
+          zIndex: -1,
         }}
       >
-        Transparent Page
+        <Webcam
+          audio={false}
+          height={size.height}
+          width={size.width}
+          videoConstraints={{ facingMode: "environment" }}
+        />
+      </div>
+      <div
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          width: "100vw",
+          height: "100vh",
+        }}
+      >
+        <p
+          style={{
+            fontSize: 60,
+            fontWeight: 800,
+            color: "white",
+            opacity: 0.5,
+          }}
+        >
+          Transparent Page
+        </p>
       </div>
     </>
   );
