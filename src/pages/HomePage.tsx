ec4aa2232883386c004ae7eae7315eecb528519e
Ashutosh <mathoreashutosh@gmail.com>
2023-03-16 00:24:00 +0530

    intital commit

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import TransparentPage from "../components/TransparentPage";
+
+const HomePage = () => {
+  return (
+    <div className="webcam">
+      <TransparentPage />
+    </div>
+  );
+};
+
+export default HomePage;
