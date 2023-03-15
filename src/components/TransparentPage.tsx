5669f7738166813c6b8f09df344fce5401ad8433
Ashutosh <mathoreashutosh@gmail.com>
2023-03-16 00:52:07 +0530

    intital commit

--- src/components/TransparentPage.tsx
+++ src/components/TransparentPage.tsx
@@ -1,18 +1,29 @@
-import React from 'react';
-import Webcam from 'react-webcam';
-import { useWindowSize } from '../hooks/useWindowSize';
+import React from "react";
+import Webcam from "react-webcam";
+import { useWindowSize } from "../hooks/useWindowSize";
 
 const TransparentPage: React.FC = () => {
-    const size = useWindowSize();
+  const size = useWindowSize();
   return (
     <>
       <Webcam
-        style={{ position: 'fixed', top: 0, left: 0, zIndex: -1 }}
+        audio={false}
+        style={{ position: "fixed", top: 0, left: 0, zIndex: -1 }}
         height={size.height}
         width={size.width}
-        videoConstraints={{ facingMode: 'user' }}
+        videoConstraints={{ facingMode: "environment" }}
       />
-      <div style={{ position: 'absolute', top: 0, left: 0, width: '100vw', height: '100vh', background: 'rgba(0, 0, 0, 0.5)', fontSize: 80 }}>
+      <div
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          width: "100vw",
+          height: "100vh",
+          background: "rgba(0, 0, 0, 0.5)",
+          fontSize: 60,
+        }}
+      >
         Transparent Page
       </div>
     </>
